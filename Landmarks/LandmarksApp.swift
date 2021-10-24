da6e7f1b1355ece5da1350c1933f35243dc10aaf
Edmirjano Preci <preci.edmirjano@gmail.com>
2021-10-24 17:01:04 +0200

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Edmirjano on 24.10.21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
